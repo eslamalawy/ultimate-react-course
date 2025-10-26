04f1096bc3e17503ccf3ee0f04c6a9ab29edebb6
eslamalawy <eslammo4kala@gmail.com>
2025-10-26 15:59:08 +0200

    Styling React Application

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,7 +1,7 @@
 // entry point must named index.js regarding webpack bundler
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./index.css";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -49,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -61,22 +61,27 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="tomato mozarella, spinach" />
-      <h2>Pizza Spinaci</h2>
-      <h2>Tomato, mozarella, spinach,</h2>
+      <h3>Pizza Spinaci</h3>
+      <p>Tomato, mozarella, spinach,</p>
     </div>
   );
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return (
+    <header className="header">
+      <h1 style={{}}>Fast React Pizza Co.</h1>
+    </header>
+  );
 }
 function Menu() {
   return (
-    <div>
-      <h2>Our menu:</h2>
+    <main className="menu">
+      <h2>Our menu</h2>
       <Pizza />
       <Pizza />
-    </div>
+    </main>
   );
 }
 function Footer() {
@@ -88,7 +93,7 @@ function Footer() {
   //   if(hour >= openHour && hour <= closeHour) alert("we'are currently open!");
   //   else alert("sorry we are closed")
 
-  return <footer>{hour}. We're currently open</footer>;
+  return <footer className="footer">{hour}. We're currently open</footer>;
   //   return React.createElement("footer",null,"We're currently open");
 }
 
