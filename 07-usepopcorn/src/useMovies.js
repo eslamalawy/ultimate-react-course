5beebe8c44ed0297b643a988d739275d93945989
eslamalawy <eslammo4kala@gmail.com>
2025-10-30 01:16:54 +0200

    07-usepopcorn

--- 07-usepopcorn/src/useMovies.js
+++ 07-usepopcorn/src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query) {
           setIsloading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
