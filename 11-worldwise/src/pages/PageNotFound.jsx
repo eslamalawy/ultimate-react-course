05f9bc8ceef4c6784287d49e0e0eb78911850537
eslamalawy <eslammo4kala@gmail.com>
2025-10-30 13:39:15 +0200

    Building the Pages

--- 11-worldwise/src/pages/PageNotFound.jsx
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
-  return <div> <PageNav /> Not found 🚦</div>;
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound;
