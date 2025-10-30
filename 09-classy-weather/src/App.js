58a17ba63854e963b02e0e2bc679d711231faeb2
eslamalawy <eslammo4kala@gmail.com>
2025-10-30 03:46:57 +0200

    Lifecycle Methods

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -36,7 +36,7 @@ async function getWeather(location) {}
 
 class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -52,6 +52,8 @@ class App extends React.Component {
   // Arrow function don't lose their binding to the "this" keywoard
   // Arrow functions don't have their own "this" keyword And instead they get access to the surrounding one
   fetchWeather = async () => {
+    if (this.state.location.length < 2)
+      return this.setState({ weather: {} });
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -76,21 +78,35 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
+  setLocation = (e) => this.setState({ location: e.target.value });
+  // useEffect []
+  componentDidMount() {
+    // this.fetchWeather()
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
 
-  setLocation = (e) => this.setState({ location: e.target.value })
+  // useEffect [location] -> this will work on mount and on re-render
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("location", this.state.location);
+    }
+  }
 
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <Input location={this.state.location} onChangeLocation={this.setLocation}/>
-        <button onClick={this.fetchWeather}>Get weather</button>
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -123,6 +139,9 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("Weather will unmount");
+  }
   render() {
     const {
       temperature_2m_max: max,
