082e9b55c975d2a7da83cc705a80c482940ce362
eslamalawy <eslammo4kala@gmail.com>
2025-10-30 03:26:30 +0200

    Child to Parent Communication.

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -43,14 +43,14 @@ class App extends React.Component {
   };
 
   // constructor(props) {
-    // super(props);
+  // super(props);
 
-    // this.fetchWeather = this.fetchWeather.bind(this);
+  // this.fetchWeather = this.fetchWeather.bind(this);
   // }
   // async fetchWeather() {...} replaced with arrow function why?
 
-    // Arrow function don't lose their binding to the "this" keywoard
-    // Arrow functions don't have their own "this" keyword And instead they get access to the surrounding one
+  // Arrow function don't lose their binding to the "this" keywoard
+  // Arrow functions don't have their own "this" keyword And instead they get access to the surrounding one
   fetchWeather = async () => {
     try {
       this.setState({ isLoading: true });
@@ -82,16 +82,14 @@ class App extends React.Component {
     }
   };
 
+
+  setLocation = (e) => this.setState({ location: e.target.value })
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <input
-          type="text"
-          placeholder="Search from location ..."
-          value={this.state.location}
-          onChange={(e) => this.setState({ location: e.target.value })}
-        />
+        <Input location={this.state.location} onChangeLocation={this.setLocation}/>
         <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
@@ -108,6 +106,21 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search from location ..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
