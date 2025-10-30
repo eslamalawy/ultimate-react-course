d428f36b99b616edb1bf23ce4433642550b473b5
eslamalawy <eslammo4kala@gmail.com>
2025-10-30 08:35:28 +0200

    Displaying Questions

--- 10-react-quiz/src/components/App.js
+++ 10-react-quiz/src/components/App.js
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   // loading  - error - ready - active - finished
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -27,7 +28,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -43,8 +47,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
-        {status === "active" && <Question />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
