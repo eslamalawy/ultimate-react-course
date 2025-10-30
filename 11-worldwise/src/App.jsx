789a82f10cfd07f11d803e49adc5217922723ad0
eslamalawy <eslammo4kala@gmail.com>
2025-10-30 14:11:13 +0200

    Nested Routes and Index Route + Outlet

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -9,11 +9,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        {/* <Route path="/" element={<Homepage />}></Route> OR index*/}
+        <Route index element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="login" element={<Login />}></Route>
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities index</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
