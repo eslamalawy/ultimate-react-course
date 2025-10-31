466a5accfb9c37d8faef7d5dee0ae1fb4797f5e9
eslamalawy <eslammo4kala@gmail.com>
2025-11-01 01:18:20 +0200

    Adding Fake Authentication Implementing Login

--- 11-worldwise/src/contexts/FakeAuthContext.jsx
+++ 11-worldwise/src/contexts/FakeAuthContext.jsx
@@ -19,7 +19,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "Eren",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
@@ -41,7 +41,7 @@ function AuthProvider({ children }) {
     dispatch({ type: "logout" });
   }
   return (
-    <AuthContext.Provider value={(user, isAuthenticated, login, logout)}>
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
