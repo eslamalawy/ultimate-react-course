024546bbf47f32b4b2e1077e7a5cf397291b0b44
eslamalawy <eslammo4kala@gmail.com>
2025-11-01 12:42:44 +0200

    trick of prop or children that do not be affacted with parent re render

--- 12-atomic-blog/src/Test.js
+++ 12-atomic-blog/src/Test.js
@@ -14,13 +14,39 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      {/* when react sees this jsx:
+        1) it will create the SlowComponent and pass it into Counter
+        2) when you click the counter
+        3) the counter rerender but the SlowComponent will not render as it already passed last time and cannot be affacted by this state update
+        4) you could also pass it as a prop it will work just the same as children
+        ex: <Counter showSlowComponent={<SlowComponent />}>  */}
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
